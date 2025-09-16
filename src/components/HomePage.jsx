9bbbacca68e642f52a112004892f2e9d60cf14bf
NBSGR . <balaganesh7944978@gmail.com>
2025-09-16 12:05:54 +0530

    f

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -33,7 +33,7 @@ const HomePage = () => {
     <div className="container">
       {/* Header */}
       <div className="header">
-        <h1>E-Commerce</h1>
+        <h1>2300031065</h1>
         <div className="header-right">
           {isAuthenticated ? (
             <div className="profile-menu">
@@ -52,7 +52,7 @@ const HomePage = () => {
             </div>
           ) : (
             <>
-              <Link to="/login"><button>Loginnnnnnnnnnnn</button></Link>
+              <Link to="/login"><button>Login</button></Link>
               <Link to="/signup"><button>Sign Up</button></Link>
             </>
           )}
